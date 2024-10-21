be8f95131d3974582e4abacb43c9f5beaf00d967
sing1871 <karanveerr.singh@mail.utoronto.ca>
2024-10-20 22:54:40 -0400

    stop service code and adjustments

--- a1/proxyServer/Orchestration/startMonitorRemote.bash
+++ a1/proxyServer/Orchestration/startMonitorRemote.bash
@@ -3,7 +3,8 @@
 
 # Start the monitor and detach the process to avoid hanging
 # ssh -o StrictHostKeyChecking=no $1 "cd /student/sing1871/CSC409/a1group05/a1/monitor/ && ./startMonitor.bash > janky-logging.txt 2>&1 & disown"
-ssh -o StrictHostKeyChecking=no $1 "cd /student/$USER/a1group05/a1/monitor/ && ./startMonitor.bash > janky-logging.txt 2>&1 & disown"
+date +"%A, %B %d, %Y %H:%M:%S.%3N" >> monitor-logging.txt
+ssh -o StrictHostKeyChecking=no $1 "cd /student/$USER/a1group05/a1/monitor/ && ./startMonitor.bash >> monitor-logging.txt 2>&1 & disown"
 # Check if the command was successful 
 if [ $? -eq 0 ]; then
     exit 0
